2b11073c2a7fccf6d057cd0617fa3df6cbb77d0a
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-04-25 10:45:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,9 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    
-    <p id="root">
-I am learning React. My life is getting better.
-</p>
-    
-    </div>
+	<h1 data-ns-test="project-name">This is my project name </h1>
+    <h4 data-ns-test="project-description">This is project description </h4>
+	</div>
   )
 }
 
